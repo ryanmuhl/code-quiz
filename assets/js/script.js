df7ee04a89dd132701c980294f03fe679666acf2
ryanmuhl <ryanmuhl@hotmail.com>
2022-02-27 09:50:29 -0500

    update assessAnswers function if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,22 @@
 var timer = document.getElementById('countdown');
 var start = document.querySelector("#start-quiz");
 var index = 0;
-var questionTitle = document.getElementById ("question-title")
-var button1Q1 = document.querySelector ("#button1")
-var button2Q1 = document.querySelector ("#button2")
-var button3Q1 = document.querySelector ("#button3")
-var butt2Q1 = document.getElementById ('button2')
-var right = document.getElementById ('result')
+var timeLeft = 60;
+var questionTitle = document.getElementById ("question-title");
+var button1 = document.querySelector ("#button1");
+var button2 = document.querySelector ("#button2");
+var button3 = document.querySelector ("#button3");
+var right = document.getElementById ('result');
 var currentQuestion;
 var points;
+
+
 
 var quizQuestions = [
 
     {
         title: "What does DOM stand for:",
-        choices: ["Domain Object Model", "Document Object Model", "Developer Oriented Model"],
+        choices: ["1) Domain Object Model", "2) Document Object Model", "3) Developer Oriented Model"],
         answer: "Document Object Model"
     },
     
@@ -34,49 +36,59 @@ var quizQuestions = [
     ];
 
 function startQuiz() {
-    
-    document.getElementById ("section").remove();
+    var mainHide = document.getElementById ("section")
+    mainHide.setAttribute ("style", "display: none")
     countdown ();
     var questionList = document.getElementById ("question-list")
     questionList.removeAttribute ("class", "hide")
     currentQuestion = quizQuestions[index]
-    populateButtons ()
+    populateQuestion ()
+    
    
     
 
     }
 
     
-    function populateButtons () {
+    function populateQuestion () {
 
-        button1Q1.textContent=currentQuestion.choices[0]
-        button2Q1.textContent=currentQuestion.choices[1]
-        button3Q1.textContent=currentQuestion.choices[2]
-        questionTitle.textContent=currentQuestion.title
-
+        button1.textContent=currentQuestion.choices[0];
+        button2.textContent=currentQuestion.choices[1];
+        button3.textContent=currentQuestion.choices[2];
+        questionTitle.textContent=currentQuestion.title;
+        
     }
+
     
     
-    button1Q1.addEventListener ("click", assessAnswers)
-    button2Q1.addEventListener ("click", assessAnswers)
-    button3Q1.addEventListener ("click", assessAnswers)
+    button1.addEventListener ("click", assessAnswers);
+    button2.addEventListener ("click", assessAnswers);
+    button3.addEventListener ("click", assessAnswers);
 
     function assessAnswers (event) {
-        console.log (event)
+        
     
-     if (event.target === button2Q1) {
+     if (event.target === button2) {
+        
         right.textContent = "Right Answer"
-        right.removeAttribute ("class", "no-buerno")
-     }
+        right.removeAttribute ("class", "no-bueno")
+        index++;
+        
 
-     else {
+    } 
+    
+    else {
+        
         right.textContent = "Wrong Answer"
-        right.removeAttribute ("class", "no-buerno")
+        right.removeAttribute ("class", "no-bueno")
+        index++;
      }
-    
-
+     startQuiz ()
 }
-    
+
+
+
+
 
 //variable to represent current question I am within
 //variable to represent how many points
@@ -94,9 +106,10 @@ function startQuiz() {
     
 // Timer that counts down from 60
 function countdown () {
-    var timeLeft = 60;
+    
 
 var timeInterval = setInterval(function () {
+    
     if (timeLeft > 0) {
 
         timer.textContent = timeLeft + ' seconds remaining';
@@ -105,6 +118,7 @@ var timeInterval = setInterval(function () {
     }else if (timeLeft === 0) {
         timer.textContent = timeLeft + ' seconds remaining';
         timeLeft--;
+        
     }else {
         timer.textcontent = '';
         alert ("See Quiz Results")
